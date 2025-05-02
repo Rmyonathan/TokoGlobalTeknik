8a495d9e2d02efcbbeffc7563ff49ec2acace61d
thomasfilemon <thomasfilemon22@gmail.com>
2025-05-02 12:13:40 +0700

    button kembali pada pembelian

--- resources/views/pembelian/lihat_nota_pembelian.blade.php
+++ resources/views/pembelian/lihat_nota_pembelian.blade.php
@@ -101,6 +101,9 @@
                         @endforelse
                     </tbody>
                 </table>
+                <div class="d-flex justify-content-center mt-4">
+                    {{ $purchases->links() }}
+                </div>
             </div>
         </div>
     </div>
