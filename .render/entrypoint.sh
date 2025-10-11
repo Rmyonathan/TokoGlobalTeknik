4384cb275fb7976ea5ab9b3e04d5a2f3707688f8
pinpin-afk <bervino66123@gmail.com>
2025-10-12 02:06:47 +0700

    update seeder

--- .render/entrypoint.sh
+++ .render/entrypoint.sh
@@ -5,9 +5,17 @@ set -e
 echo "Running migrations for main database..."
 php artisan migrate --force
 
+# Jalankan seeder untuk DB utama
+echo "Running seeders for main database..."
+php artisan db:seed --force
+
 # # Jalankan migrasi untuk DB kedua
 # echo "Running migrations for secondary database..."
 # php artisan migrate --database=second_mysql --force
+
+# Jalankan seeder untuk DB kedua
+echo "Running seeders ..."
+php artisan db:seed
 
 # Jalankan perintah utama container (Apache)
 exec apache2-foreground
