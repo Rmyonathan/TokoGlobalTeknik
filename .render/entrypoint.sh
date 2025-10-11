683cffec61a539ff94e9812277d7c3a0b7e66e73
pinpin-afk <bervino66123@gmail.com>
2025-10-12 01:32:17 +0700

    update sell

--- .render/entrypoint.sh
+++ .render/entrypoint.sh
@@ -5,9 +5,9 @@ set -e
 echo "Running migrations for main database..."
 php artisan migrate --force
 
-# Jalankan migrasi untuk DB kedua
-echo "Running migrations for secondary database..."
-php artisan migrate --database=second_mysql --force
+# # Jalankan migrasi untuk DB kedua
+# echo "Running migrations for secondary database..."
+# php artisan migrate --database=second_mysql --force
 
 # Jalankan perintah utama container (Apache)
 exec apache2-foreground
